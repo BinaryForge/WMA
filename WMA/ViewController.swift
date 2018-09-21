636034bc3af9c31176c7cf67a4bf76abf91f0294
Alexey Kuznetsov <alexeykzntsv@yahoo.com>
2018-09-21 09:19:27 -0700

    Initial Commit

---
+++ WMA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WMA
+//
+//  Created by Alexey Kuznetsov on 9/21/18.
+//  Copyright © 2018 Alexey Cain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
