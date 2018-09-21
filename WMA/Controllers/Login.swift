fecf22bb9191101ba83c7884b2e242cf9c7b6ca9
Alexey Kuznetsov <alexeykzntsv@yahoo.com>
2018-09-21 14:50:09 -0700

    Initial Commit

---
+++ WMA/Controllers/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  WMA
+//
+//  Created by Alexey Kuznetsov on 9/21/18.
+//  Copyright © 2018 Alexey Cain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Login: UIViewController{
+    
+    
+    
+    
+    
+}
---
+++ WMA/Controllers/Register.swift
@@ -0,0 +1,14 @@
+//
+//  SignUP.swift
+//  WMA
+//
+//  Created by Alexey Kuznetsov on 9/21/18.
+//  Copyright © 2018 Alexey Cain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Register: UIViewController  {
+    
+}
