52bd9ec429e436e64dc12919abf2c5f06be922fa
Alexey Cain <alpha.valleyforge@gmail.com>
2018-10-02 13:01:11 -0700

    Implemented user input for employee/employer

--- WMA/Controllers/SignUp.swift
+++ WMA/Controllers/SignUp.swift
@@ -11,20 +11,42 @@ import Firebase
 import FirebaseAuth
 
 class SignUp: UIViewController{
-
+    
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
-    
-    
-
-    
-    
-    
-    
+    var employeeCheck: Bool?
+    var employerCheck: Bool?
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
+    @IBAction func employeeButton(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+            employeeCheck = sender.isSelected
+           
+        }else{
+            sender.isSelected = true
+            employeeCheck = sender.isSelected
+        }
+    }
+   
+    @IBAction func employeerButton(_ sender: UIButton) {
+        if sender.isSelected{
+            sender.isSelected = false
+            employerCheck = sender.isSelected
+            
+        }else{
+            sender.isSelected = true
+            employerCheck = sender.isSelected
+        }
+    }
+    
+    
+    
         @IBAction func registerPressed(_ sender: UIButton) {
             if emailText.text!.isEmpty &&  passwordText.text!.isEmpty{
                 let alert = UIAlertController(title: "Missing Credentials", message: "Please enter email & password.", preferredStyle: .alert)
@@ -33,7 +55,19 @@ class SignUp: UIViewController{
                 }))
                 self.present(alert, animated: true, completion: nil)
             }
-            print("Enter")
+            print("Employee has been checked", employeeCheck)
+            print("Employer has been checked", employerCheck)
+            
+            if employeeCheck == true && employerCheck == true{
+                let alert = UIAlertController(title: "Position Error", message: "Cannot be both Employer and Employee", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"Missing Credentials\" alert occured.")
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            
+            
             Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (user, error) in
                 if error != nil{
                     if let errCode = AuthErrorCode(rawValue: error!._code) {
